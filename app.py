5ba688ce53475c71c31a9e70fdbe5cff58baba9b
josiahadrineda <jnsa.tech@gmail.com>
2020-09-27 22:38:15 -0700

    Minor debugging

--- app.py
+++ app.py
@@ -6,13 +6,6 @@ from ArticleScraper import *
 from NearbyCities import *
 
 
-proxies = {
-    'http': '87.126.43.160:8080',
-    'http':'212.154.58.99:37470',
-    'http':'134.122.124.106:3128',
-}
-
-
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -20,11 +13,10 @@ app.config["DEBUG"] = True
 @app.route('/', methods=['GET'])
 def home():
     return '''<h1>Monkeys write a code that save California </h1>
-<p>We are writing a webscraper that compiles data on local fires</p>'''
+    <p>We are writing a webscraper that compiles data on local fires</p>'''
 
 
 @app.route('/api/articles', methods=['GET'])
-  # article scraper
 def article_info():
     city = request.args.get('city')
     n = int(request.args.get('n'))
@@ -41,7 +33,7 @@ def title_Finder():
 @app.route('/api/nearCities', methods=['GET'])
 def get_nearby_cities():
     city = request.args.get('city')
-    k = request.args.get('k')
+    k = int(request.args.get('k'))
 
     return nearby_cities(city, k)
 
